7e5449065bef37bbc131e6bb330e7fc092c208b8
Sandeep <sandy.joshi5@gmail.com>
2017-08-12 11:06:15 +0530

    initial Commit

---
+++ movieJsonData.js
@@ -0,0 +1,26 @@
+var bestPictures = [{
+	"year"	: "1996",
+	"value"	: "Taurus Film",
+	"id" 	: "20555",
+	"img" 	: "http://www.articleonepartners.com/wp-content/uploads/2015/06/1000px-Clapboard.svg_-300x300.png",
+	"url"	: "http://example.com"
+}, {
+	"year"	: "1998",
+	"value"	: "Linson Films",
+	"id" 	: "54050",
+	"img" 	: "http://clipartix.com/wp-content/uploads/2016/04/Movie-night-clipart-free-clipart-images.jpeg",
+	"url"	: "http://example.com"
+}, {
+	"year"	: "1999",
+	"value"	: "Fox 2000 Picture",
+	"id" 	: "711",
+	"img" 	: "http://clipartix.com/wp-content/uploads/2016/04/Movie-clipart-black-and-white-2.png",
+	"url"	: "http://example.com"
+}
+, {
+	"year"	: "1989",
+	"value"	: "Regency Enterprises",
+	"id" 	: "508",
+	"img" 	: "http://clipartix.com/wp-content/uploads/2016/04/Movie-reel-movie-film-camera-clipart-image-clipartcow.jpg",
+	"url"	: "http://example.com"
+}];
